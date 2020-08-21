fd7c82f9a1c1b69c64a3a20ad1e29018f549cdf4
amacken <alex.mackenzie2@gmail.com>
2020-08-21 12:07:57 -0400

    Added new, update, and create routes. New ball view also created. New ball entries now posting to index page

--- controllers/balls.js
+++ controllers/balls.js
@@ -13,12 +13,35 @@ router.get('/', (req, res) => {
 });
 
 // New
+router.get('/new', (req, res) => {
+    res.render('balls/New')
+});
 
 // Delete
 
 // Update
+router.put('/:id', (req, res) => {
+    req.body.hallOfFamer = req.body.hallOfFamer === "on" ? true : false;
+    Ball.findByIdAndUpdate(req.params.id, req.body, { new: true }, (error, updatedModel) => {
+        res.redirect('/balls');
+    });
+});
 
 // Create
+router.post('/', (req, res) => {
+    if (req.body.hallOfFamer === "on") {
+        req.body.hallOfFamer = true;
+    } else {
+        req.body.hallOfFamer = false;
+    }
+    Ball.create(req.body, (error, createdBall) => {
+        if (error) {
+            console.log('error', error)
+        } else {
+            res.redirect('/balls');
+        }
+    });
+});
 
 // Edit
 
---
+++ views/balls/New.jsx
@@ -0,0 +1,21 @@
+const React = require('react');
+const Default = require('../components/Default')
+
+class New extends React.Component {
+    render() {
+      return (
+          <Default>
+              <h1>New Ball Page</h1>
+              <form action="/balls" method="POST">
+                  Name: <input type="text" name="name" /><br/>
+                  Image: <input type="text" name="img" /><br/>
+                  Hall of Famer: <input type="checkbox" name="hallOfFamer" /><br/>
+                  Purchase Price: <input type="number" name="purchasePrice" /><br/>
+                  Certification Number: <input type="text" name="certificationNumber" /><br/>
+                  <input type="submit" name="" value="Create Ball Entry"/>
+               </form>
+          </Default>);
+    }
+  }
+  
+  module.exports = New;
